bee88358011eda7b9803911c4f9f70919a03b318
mabutamail <mabutamail@gmail.com>
2019-10-06 13:42:14 +0300

    Initial commit

---
+++ src/sql/demo/StockExchangeDB.java
@@ -0,0 +1,56 @@
+package sql.demo;
+
+import sql.demo.model.Traider;
+import sql.demo.repository.*;
+import java.sql.*;
+
+public class StockExchangeDB {
+    // Блок объявления констант
+    public static final String DB_URL = "jdbc:h2:/f:/Java/Projects/SQL_JDBC/db/stockExchange";
+    public static final String DB_Driver = "org.h2.Driver";
+
+    // Таблицы СУБД
+    Traiders traiders;
+    ShareRates shareRates;
+    Shares shares;
+    TraiderShareActions traiderShareActions;
+
+    // Получить новое соединение с БД
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL);
+    }
+
+    // Инициализация
+    public StockExchangeDB() throws SQLException, ClassNotFoundException {
+        Class.forName(DB_Driver);
+        // Инициализируем таблицы
+        traiders = new Traiders();
+        shares = new Shares();
+        shareRates = new ShareRates();
+        traiderShareActions = new TraiderShareActions();
+    }
+
+    // Создание всех таблиц и ключей между ними
+    public void createTablesAndForeignKeys() throws SQLException {
+        shares.createTable();
+        shareRates.createTable();
+        traiders.createTable();
+        traiderShareActions.createTable();
+        // Создание внешних ключей (связь между таблицами)
+        shareRates.createForeignKeys();
+        traiderShareActions.createForeignKeys();
+    }
+
+
+    public static void main(String[] args) {
+        try{
+            StockExchangeDB stockExchangeDB = new StockExchangeDB();
+            stockExchangeDB.createTablesAndForeignKeys();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Ошибка SQL !");
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC драйвер для СУБД не найден!");
+        }
+    }
+}
